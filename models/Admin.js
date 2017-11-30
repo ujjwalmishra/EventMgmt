607df9e1799242a8dbf771f7ba1ac52c83f16310
Ujjawal <ujjwalmishra7@gmail.com>
2017-11-30 17:22:17 -0600

    admin model

---
+++ models/Admin.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const adminSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  merchants: [{ type: Schema.Types.ObjectId, ref: 'Merchant' }]
+}, { timestamps: true });
+
+/**
+ * Password hash middleware.
+ */
+adminSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+adminSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+/**
+ * Helper method for getting user's gravatar.
+ */
+adminSchema.methods.gravatar = function gravatar(size) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
+const Admin = mongoose.model('User', adminSchema);
+
+module.exports = Admin;
